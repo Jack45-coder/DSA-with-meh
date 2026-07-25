3183c64f0d038d809412337cd8773597acf5fb71
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-07-25 23:19:44 +0530

    generic

---
+++ src/generic/MyGeneric.java
@@ -0,0 +1,29 @@
+package generic;
+
+public class MyGeneric {
+    public static void main(String[] args) {
+        Displayer<Integer> IntegerObj = new Displayer<>();
+        IntegerObj.setNum(20);
+        IntegerObj.display();
+
+        Displayer<Double> doubleObj = new Displayer<>();
+        doubleObj.setNum(20.099);
+        doubleObj.display();
+    }
+}
+
+    class Displayer<T>{
+        T num;
+
+        public T getNum() {
+            return num;
+        }
+
+        public void setNum(T num) {
+            this.num = num;
+        }
+
+        public void display(){
+            System.out.println(num);
+        }
+    }
