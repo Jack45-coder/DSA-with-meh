92a6ce04ec7d60df7c3c8b6f5779b43fc1849f01
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-07-27 00:46:43 +0530

    updatE

--- src/collectionFramework/comparator/Main.java
+++ src/collectionFramework/comparator/Main.java
@@ -10,7 +10,7 @@ public class Main {
         List<Employee> employees = new ArrayList<>();
         employees.add(new Employee(3, "Ravish", 76000.00));
         employees.add(new Employee(1, "Satish", 56000.00));
-        employees.add(new Employee(4, "Rohit", 66000.00));
+        employees.add(new Employee(4, "Rohit", 96000.00));
         employees.add(new Employee(2, "Prince", 57000.00));
         employees.add(new Employee(5, "Deepak", 96000.00));
 
@@ -39,5 +39,24 @@ public class Main {
         for(Employee employee : employees){
             System.out.println("Employee: " + employee.name + " | Salary: " + employee.salary);
         }
+
+
+        // cond -> sort in dec order of salary
+        // if salary is same then sort in increasing order of name
+        System.out.println("After: Sorted salary first Then name");
+        Collections.sort(employees, new Comparator<Employee>(){
+            @Override
+            public int compare(Employee e1, Employee e2) {
+                if (e1.salary != e2.salary){
+                    return Double.compare(e2.salary, e1.salary);
+                }
+                return e1.name.compareTo(e2.name);
+            }
+        });
+
+        for(Employee employee : employees){
+            System.out.println("Employee: " + employee.name + " | Salary: " + employee.salary);
+        }
+
     }
 }
