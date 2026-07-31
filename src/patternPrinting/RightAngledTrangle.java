3af6f0f0f370d63657f7187be74098644ecd5b63
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-08-01 00:11:17 +0530

    updatE

--- src/patternPrinting/RightAngledTrangle.java
+++ src/patternPrinting/RightAngledTrangle.java
@@ -12,10 +12,26 @@ public class RightAngledTrangle {
             System.out.println();
         }
 
-        System.out.println("Number Right Angled Trangle");
+        System.out.println("Inverted Right Angled Trangle");
+        for (int i = 0; i < r; i++){
+            for (int j = 0; j < r-i; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Number Based Right Angled Trangle");
         for (int i = 1; i <= r; i++){
             for (int j = 1; j <= i; j++){
                 System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        System.out.println("Character Based Right Angled Trangle");
+        for (int i = 0; i < r; i++){
+            for (int j = 0; j <= i; j++){
+                System.out.print((char)(65 + j));
             }
             System.out.println();
         }
