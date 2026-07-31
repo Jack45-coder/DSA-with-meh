62ec72197a8519be585f5878fd8335651fc84761
Jackey Kumar <jackeyjazzbgp1234@gmail.com>
2026-07-31 23:40:37 +0530

    Add rightAngledTrangle pattern

--- src/patternPrinting/RightAngledTrangle.java
+++ src/patternPrinting/RightAngledTrangle.java
@@ -1,4 +1,15 @@
 package patternPrinting;
 
 public class RightAngledTrangle {
+    public static void main(String[] args) {
+        int r = 4;
+
+        for (int i = 1; i <= r; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
 }
